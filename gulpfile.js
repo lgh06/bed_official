969f8a8a9bfc2a04ac7345e22dab42053aac53ef
Gehuan Liu <hnnk@qq.com>
2015-09-30 10:37:51 +0800

    modify gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,29 @@
 var gulp = require('gulp');
 
 gulp.task('default', function() {
-   //将你的默认的任务代码放在这
+   //
 });
 
 
 
 var less = require('gulp-less');
 var path = require('path');
+
+
+var postcss      = require('gulp-postcss');
+var sourcemaps   = require('gulp-sourcemaps');
+var autoprefixer = require('autoprefixer');
  
 gulp.task('one', function () {
   return gulp.src('./css/bedtime.less')
     .pipe(less({
-      paths: [ path.join(__dirname, 'less', 'includes') ]
+      paths: [ path.join(__dirname)]//, 'less', 'includes') ]
     }))
+    .pipe(postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
     .pipe(gulp.dest('./css'));
 });
 
 gulp.task('two', function () {
-    var postcss      = require('gulp-postcss');
-    var sourcemaps   = require('gulp-sourcemaps');
-    var autoprefixer = require('autoprefixer');
  
     return gulp.src('./css/*.css')
         //.pipe(sourcemaps.init())
